6faaf4a8c46bcab70e870a687a849cdb3f80a8a9
Alex <1@r.rr>
2019-07-28 16:47:53 +0300

    Lesson10 Timer - addZero

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -1,18 +1,22 @@
+//classes hide-show seted in style.css
+//fade - animation
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
     let tab = document.querySelectorAll('.info-header-tab'),
-        info = document.querySelector('.info-header'),
-        tabContent = document.querySelectorAll('.info-tabcontent');
+    info = document.querySelector('.info-header'),
+    tabContent = document.querySelectorAll('.info-tabcontent');
 
     function hideTabContent(a) {
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
+            
         }
     }
-
     hideTabContent(1);
+
+					  
 
     function showTabContent(b) {
         if (tabContent[b].classList.contains('hide')) {
@@ -20,66 +24,70 @@ window.addEventListener('DOMContentLoaded', function() {
             tabContent[b].classList.add('show');
         }
     }
+    showTabContent(0);
 
-    info.addEventListener('click', function(event) {
+    info.addEventListener('click',function(event){
         let target = event.target;
-        if (target && target.classList.contains('info-header-tab')) {
-            for(let i = 0; i < tab.length; i++) {
-                if (target == tab[i]) {
+        if(target && target.classList.contains('info-header-tab')){
+            for (let i = 0; i < tab.length; i++) {
+                if(target == tab[i]){
                     hideTabContent(0);
                     showTabContent(i);
                     break;
                 }
+                
             }
         }
 
-    })
+    });
+    //timer
+    let deadline = '2019-09-29';
 
-    let deadline = '2018-11-21';
-
-    function getTimeRemaining(endtime) {
-        let t = Date.parse(endtime) - Date.parse(new Date()),
+    function getTimeRemaining(endtime){
+        let t = Date.parse(endtime)-Date.parse(new Date()), //Date.parse - return date in milliseconds
         seconds = Math.floor((t/1000) % 60),
         minutes = Math.floor((t/1000/60) % 60),
         hours = Math.floor((t/(1000*60*60)));
-
+        //hours = Math.floor((t/(1000/60/60) % 24)),
+        //days = Math.floor((t/(1000*60*60*24)))
         return {
-            'total' : t,
-            'hours' : hours,
-            'minutes' : minutes,
-            'seconds' : seconds
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds':seconds
         };
     }
 
-    function setClock(id, endtime) {
+    function setClock(id,endtime){
         let timer = document.getElementById(id),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
-            timeInterval = setInterval(updateClock, 1000);
-            
-        function updateClock() {
+            timeInterval = setInterval(updateClock,1000);
+
+        function updateClock(){
             let t = getTimeRemaining(endtime);
 
             function addZero(num){
-                        if(num <= 9) {
-                            return '0' + num;
-                        } else return num;
-                    };
-
+                if(num <= 9) {
+                    return '0' + num;
+                } else return num;
+            }
+								  
             hours.textContent = addZero(t.hours);
             minutes.textContent = addZero(t.minutes);
-            seconds.textContent = addZero(t.seconds);
+            seconds.textContent = addZero(t.seconds);									  
 
-            if (t.total <= 0) {
+            if (t.total <= 0){
                 clearInterval(timeInterval);
                 hours.textContent = '00';
                 minutes.textContent = '00';
-                seconds.textContent = '00';
+                seconds.textContent = '00';									   
+										   
             }
         }
-
     }
 
-    setClock('timer', deadline);
+    setClock('timer',deadline);
+
 });
